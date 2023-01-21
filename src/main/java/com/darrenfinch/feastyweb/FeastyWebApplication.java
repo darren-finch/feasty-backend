717ddad4d8e060b956990aa767e46eb9bcebf039
Darren Finch <finchdarren25@gmail.com>
2023-01-21 11:06:42 -0600

    Initial project setup

---
+++ src/main/java/com/darrenfinch/feastyweb/FeastyWebApplication.java
@@ -0,0 +1,13 @@
+package com.darrenfinch.feastyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeastyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeastyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darrenfinch/feastyweb/FeastyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darrenfinch.feastyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
