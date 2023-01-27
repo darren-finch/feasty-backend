de4a8505e2064e93a83d52564356e18f450b1c76
Darren Finch <finchdarren25@gmail.com>
2023-01-27 06:08:17 -0600

    Loosen security policy for testing & add basic screens with routing

--- src/main/java/com/darrenfinch/feastyweb/FeastyWebApplication.java
+++ src/main/java/com/darrenfinch/feastyweb/FeastyWebApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FeastyWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FeastyWebApplication.class, args);
 	}
-
 }
